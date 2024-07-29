217a347afbf7cf9ee8731abd39be1ded8544c71a
s-tanaka <k1j8b6z5i7j9g2f5@cloudpack.slack.com>
2024-07-29 17:29:06 +0900

    Initial commit

---
+++ test/code_review.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CodeReview from '../lib/code_review-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/code_review-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CodeReview.CodeReviewStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
